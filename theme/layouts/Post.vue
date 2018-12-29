0d25726f315207632fd192308333a3609d37dc4d
Eliaztray <944126009@qq.com>
2018-12-29 11:34:09 +0800

    feat: tweaks

--- theme/layouts/Post.vue
+++ theme/layouts/Post.vue
@@ -2,11 +2,17 @@
   <Layout>
     <div class="custom-page-post">
       <nav class="breadcrumbs">
-        <div class="page-size">
+        <div class="page-meta">
           <span>文章字数约:</span>
           <span>{{$frontmatter.pageSize}} words</span>
           <span class="page-pre">预计阅读:</span>
           <span>{{$frontmatter.readTime}} min</span>
+        </div>
+        <div class="page-meta">
+          <span>文章创建于:</span>
+          <span>{{getCreateTime(cur)}}</span>
+          <span class="page-pre">最后修改:</span>
+          <span>{{getModifyTime(cur)}}</span>
         </div>
         <ul class="rec-list is-category" v-if="categoryList.length">
           <li class="item" v-for="(category, index) in categoryList" :key="index">
@@ -26,8 +32,9 @@
       <Content class="post-content" />
       <!-- prev & next -->
       <div class="post-nav" v-if="prev || next">
-        <div class="prev" v-if="prev"><a :href="prev.path">⬅️ {{prev.title || prev.path}}</a></div>
-        <div class="next" v-if="next"><a :href="next.path">➡️ {{next.title || next.path}}</a></div>
+        <!-- TODO: add tooltip & animation -->
+        <div class="post-prev" v-if="prev"><a :href="prev.path">{{prev.title || prev.path}}</a></div>
+        <div class="post-next" v-if="next"><a :href="next.path">{{next.title || next.path}}</a></div>
       </div>
       <!-- comments component-->
       <comments />
@@ -38,6 +45,7 @@
 <script>
 import get from 'lodash-es/get'
 import dayjs from 'dayjs'
+const FORMAT_TEMPLATE = 'YYYY-MM-DD hh:mm:ss'
 export default {
   name: 'Post',
   filters: {
@@ -59,11 +67,17 @@ export default {
     find(path, offset) {
       return this.postFilter[this.postFilter.findIndex(post => post.path === decodeURIComponent(path)) + offset]
     },
-    // 取每个页面 frontmatter 定义的创建时间(date), 无定义的话取每个页面的 git 记录上的时间戳，再无则取当前时间
-    // 按最新时间排列
+    // Get time format with `FORMAT_TEMPLATE`
+    getCreateTime(post) {
+      return dayjs(get(post.frontmatter, 'date', this.getModifyTime(post))).format(FORMAT_TEMPLATE)
+    },
+    getModifyTime(post) {
+      return dayjs(get(post, 'lastUpdated', dayjs())).format(FORMAT_TEMPLATE)
+    },
+    // 按创建的最新时间排列
     sortByTime(a, b) {
-      const aTime = dayjs(get(a.frontmatter, 'date', get(a, 'lastUpdated', Date.now()))).valueOf()
-      const bTime = dayjs(get(b.frontmatter, 'date', get(b, 'lastUpdated', Date.now()))).valueOf()
+      const aTime = dayjs(this.getCreateTime(a)).valueOf()
+      const bTime = dayjs(this.getCreateTime(b)).valueOf()
       return bTime - aTime
     }
   },
@@ -72,7 +86,7 @@ export default {
       return Object.entries(this.$categories._metaMap).reduce((result, item) => {
         const key = item[0]
         let value = get(item[1], 'posts')
-        if (value.find(post => post.path === this.$route.path)) {
+        if (value.find(post => post.path === decodeURIComponent(this.$page.path))) {
           result.push({
             key,
             path: get(item[1], 'path')
@@ -85,7 +99,7 @@ export default {
       return Object.entries(this.$tags._metaMap).reduce((result, item) => {
         const key = item[0]
         let value = get(item[1], 'posts')
-        if (value.find(post => post.path === this.$route.path)) {
+        if (value.find(post => post.path === decodeURIComponent(this.$page.path))) {
           result.push({
             key,
             path: get(item[1], 'path')
@@ -118,6 +132,9 @@ export default {
         return undefined
       }
       return this.resolvePrev(this.$page.path)
+    },
+    cur() {
+      return this.postFilter.find(item => item.path === decodeURIComponent(this.$page.path))
     }
   }
 }
@@ -126,13 +143,14 @@ export default {
 
 <style src="prismjs/themes/prism-tomorrow.css">
 </style>
-<style lang="stylus" src="@parent-theme/styles/theme.styl">
-</style>
+
 <style lang="scss">
 @import '../styles/theme.scss';
 </style>
 
 <style lang="scss">
+@import '../styles/mixin.scss';
+
 .breadcrumbs {
   padding: 0 10px;
   margin-left: 50%;
@@ -169,7 +187,7 @@ export default {
   }
 }
 
-.page-size {
+.page-meta {
   padding: 6px 0;
   color: #3eaf7c;
   font-size: 0.8rem;
@@ -184,11 +202,21 @@ export default {
 }
 
 .post-nav {
-  display: flex;
+  overflow: hidden;
   max-width: 740px;
-  align-items: center;
-  justify-content: space-between;
+  padding: 1rem 0;
   margin: 0 auto;
+}
+
+.post-prev {
+  @include textOverFlow();
+
+  max-width: 200px;
+  float: left;
+}
+
+.post-next {
+  float: right;
 }
 </style>
 
